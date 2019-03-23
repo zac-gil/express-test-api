34d15a32f33fee3d99c49b8566e85262d801c6f8
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-03-23 15:11:04 +0000

    Multiline arrow function example

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/anotherone', (req, res) => res.send('Another One'))
+app.get('/anotherone', (req, res) => {
+    res.send('Another One')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
