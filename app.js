5ccf5511a2100d1aca6849b099c052c26185562e
Zac Galliott <zac.galliott@smilemachine.com>
2019-03-23 15:17:45 +0000

    Added dictionary

--- app.js
+++ app.js
@@ -11,4 +11,17 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/anotherone', (req, res) => res.send('Another One'))
 
+app.get('/users', (req, res) => res.send(Object.values(users)))
+
+let users = [
+  {
+    "id": "id1",
+    "username":"Someone"
+  },
+  {
+    "id": "AnotherID",
+    "username":"SomeoneElse"
+  }
+]
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
