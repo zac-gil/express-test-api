b2341f4d975930c95e2949b66939a389d78efc03
Zac Galliott <zac.galliott@smilemachine.com>
2019-03-23 15:04:39 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const routes = require("./routes/routes.js")
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/anotherone', (req, res) => res.send('Another One'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
